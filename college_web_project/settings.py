dc72863a544664aec87f44ef15cbf3885efa7e92
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2025-03-15 11:05:19 +0530

    Second commit

--- college_web_project/settings.py
+++ college_web_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +25,8 @@ SECRET_KEY = 'django-insecure-xd8jmof^jr&1r-jie3@a0$@z3tk_$03_*t-=#ohnkp-g-2(-mr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
